fbc524b128d552465cb36488adf84a112ebc754f
Pierre Tabet <pierre.tabet@me.com>
2024-07-02 10:47:05 +0100

    manual match_appliances

--- utils/parse_and_match.py
+++ utils/parse_and_match.py
@@ -23,6 +23,34 @@ def match_appliances(user_appliances, predefined_appliances):
                     'Matched Appliance': None,
                     'Match Found': False
                 })
+        elif user_appliance['name'] == "Blue Seal G570":
+            specific_match = potential_matches[potential_matches['name'].str.contains("Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088", case=False)]
+            if not specific_match.empty:
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
+                    'Matched Appliance': specific_match.iloc[0].to_dict(),
+                    'Match Found': True
+                })
+            else:
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
+                    'Matched Appliance': None,
+                    'Match Found': False
+                })
+        elif user_appliance['name'] == "Blue Seal G570":
+            specific_match = potential_matches[potential_matches['name'].str.contains("Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088", case=False)]
+            if not specific_match.empty:
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
+                    'Matched Appliance': specific_match.iloc[0].to_dict(),
+                    'Match Found': True
+                })
+            else:
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
+                    'Matched Appliance': None,
+                    'Match Found': False
+                })
         else:
             if not potential_matches.empty:
                 matched_appliances.append({
