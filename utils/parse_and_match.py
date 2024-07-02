3cc7c651694864ef66ff1a1fa7b7288a1f10719f
Pierre Tabet <pierre.tabet@me.com>
2024-07-02 10:31:26 +0100

    chore: Refactor match_appliances function for improved readability and performance

--- utils/parse_and_match.py
+++ utils/parse_and_match.py
@@ -4,43 +4,40 @@ user_appliances_df = pd.read_csv("uploads/user_upload.csv")
 predefined_appliances_df = pd.read_csv("./appliances.csv")
 
 def match_appliances(user_appliances, predefined_appliances):
-    matched_appliances = pd.DataFrame(columns=['User Appliance', 'Matched Appliance', 'Match Found'])
-    matched_list = []
+    matched_appliances = []
 
-    for index, user_appliance in user_appliances.iterrows():
+    for _, user_appliance in user_appliances.iterrows():
         potential_matches = predefined_appliances[predefined_appliances['tag'] == user_appliance['tag']]
         
         if user_appliance['name'] == "Ideal Logic2 C35 35kW Combination Boiler Natural Gas ErP":
             specific_match = potential_matches[potential_matches['name'].str.contains("tepeo", case=False)]
             if not specific_match.empty:
-                matched_list.append({
-                    'User Appliance': user_appliance['name'],
-                    'Matched Appliance': specific_match['name'].iloc[0],
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
+                    'Matched Appliance': specific_match.iloc[0].to_dict(),
                     'Match Found': True
                 })
             else:
-                matched_list.append({
-                    'User Appliance': user_appliance['name'],
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
                     'Matched Appliance': None,
                     'Match Found': False
                 })
         else:
             if not potential_matches.empty:
-                matched_list.append({
-                    'User Appliance': user_appliance['name'],
-                    'Matched Appliance': potential_matches['name'].iloc[0],  
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
+                    'Matched Appliance': potential_matches.iloc[0].to_dict(),
                     'Match Found': True
                 })
             else:
-                matched_list.append({
-                    'User Appliance': user_appliance['name'],
+                matched_appliances.append({
+                    'User Appliance': user_appliance.to_dict(),
                     'Matched Appliance': None,
                     'Match Found': False
                 })
 
-    matched_appliances = pd.concat([matched_appliances, pd.DataFrame(matched_list)], ignore_index=True)
-    return matched_appliances
+    matched_appliances_df = pd.DataFrame(matched_appliances)
+    return matched_appliances_df
 
 matched_appliances_df = match_appliances(user_appliances_df, predefined_appliances_df)
-print(matched_appliances_df['User Appliance'])
-print(matched_appliances_df['Matched Appliance'])
