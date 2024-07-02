c24887d0d91f8d1a8ae9079eb106764621262a9d
Jiuming Qin <jiuming.qin@gmail.com>
2024-07-02 12:26:32 +0100

    appliances gas/electric clf

--- utils/parse_and_match.py
+++ utils/parse_and_match.py
@@ -3,14 +3,26 @@ import pandas as pd
 user_appliances_df = pd.read_csv("uploads/user_upload.csv")
 predefined_appliances_df = pd.read_csv("./appliances.csv")
 
+
+def get_specific_matches(potential_matches, name_string, user_appliance):
+    return potential_matches[
+        (potential_matches['name'].str.contains(name_string, case=False))
+        & (potential_matches['daily_usage_hours'] == user_appliance['daily_usage_hours'])
+    ]
+
+
 def match_appliances(user_appliances, predefined_appliances):
     matched_appliances = []
 
     for _, user_appliance in user_appliances.iterrows():
         potential_matches = predefined_appliances[predefined_appliances['tag'] == user_appliance['tag']]
-        
-        if user_appliance['name'] == "Ideal Logic2 C35 35kW Combination Boiler Natural Gas ErP":
-            specific_match = potential_matches[potential_matches['name'].str.contains("tepeo", case=False)]
+        if user_appliance['name'] == "Ideal Logic2 C35 35kW Combination Boiler Natural Gas ErP" :
+            # specific_match = potential_matches[
+            #     (potential_matches['name'].str.contains("tepeo", case=False)) &
+            #     (potential_matches['Daily_Usage_Hours'] == user_appliance['Daily_Usage_Hours'])
+            # ]
+            name_string = "tepeo"
+            specific_match = get_specific_matches(potential_matches, name_string, user_appliance)
             if not specific_match.empty:
                 matched_appliances.append({
                     'User Appliance': user_appliance.to_dict(),
@@ -24,7 +36,12 @@ def match_appliances(user_appliances, predefined_appliances):
                     'Match Found': False
                 })
         elif user_appliance['name'] == "Blue Seal G570":
-            specific_match = potential_matches[potential_matches['name'].str.contains("Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088", case=False)]
+            # specific_match = potential_matches[
+            #     (potential_matches['name'].str.contains("Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088", case=False)) &
+            #     (potential_matches['Daily_Usage_Hours'] == user_appliance['Daily_Usage_Hours'])
+            # ]
+            name_string = "Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088"
+            specific_match = get_specific_matches(potential_matches, name_string, user_appliance)
             if not specific_match.empty:
                 matched_appliances.append({
                     'User Appliance': user_appliance.to_dict(),
@@ -38,7 +55,8 @@ def match_appliances(user_appliances, predefined_appliances):
                     'Match Found': False
                 })
         elif user_appliance['name'] == "Blue Seal G570":
-            specific_match = potential_matches[potential_matches['name'].str.contains("Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088", case=False)]
+            name_string = "Blue Seal E31D4 Electric Convection Oven Turbofan 95 Litre Digital - CE088"
+            specific_match = get_specific_matches(potential_matches, name_string, user_appliance)
             if not specific_match.empty:
                 matched_appliances.append({
                     'User Appliance': user_appliance.to_dict(),
@@ -69,3 +87,4 @@ def match_appliances(user_appliances, predefined_appliances):
     return matched_appliances_df
 
 matched_appliances_df = match_appliances(user_appliances_df, predefined_appliances_df)
+# print(matched_appliances_df)
--- utils/running_cost.py
+++ utils/running_cost.py
@@ -1,5 +1,3 @@
-import os.path
-
 import pandas as pd
 import numpy as np
 import argparse
@@ -11,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 def load_carbon_intensity():
-    df = pd.read_csv("../octopus_consumption.csv", parse_dates=True)[["Carbon Intensity (gCO2/kWh)"]]
+    df = pd.read_csv("./octopus_consumption.csv", parse_dates=True)[["Carbon Intensity (gCO2/kWh)"]]
     df['timestamp'] = df.index.to_series().dt.time.astype(str)
     carbon_intensity = (
         df
@@ -23,7 +21,7 @@ def load_carbon_intensity():
     return carbon_intensity
 
 
-def half_hour_running_cost(usage:pd.DataFrame, tariffs:pd.DataFrame, economy:str='Economy 5'):
+def half_hour_running_cost(usage:pd.DataFrame, tariffs:pd.DataFrame, usage_column='primaryValue', economy:str='Economy 5'):
     usage['timestamp_day'] = pd.to_datetime(usage['timestamp']).dt.time.astype(str)
     carbon_intensity = load_carbon_intensity()
     usage = (
@@ -42,12 +40,12 @@ def half_hour_running_cost(usage:pd.DataFrame, tariffs:pd.DataFrame, economy:str
             how='left',
         )
     )
-    usage['cost'] = usage['primaryValue'] * usage['unit_price']
-    usage['carbon_emission'] = usage['primaryValue'] * usage['carbon_intensity']
+    usage['cost'] = usage[usage_column] * usage['unit_price']
+    usage['carbon_emission'] = usage[usage_column] * usage['carbon_intensity']
     return usage
 
 
-def projected_yearly_cost_range(appliance:pd.Series, half_hour_cost:pd.DataFrame):
+def projected_yearly_cost_range_single_appliance(appliance:pd.Series, half_hour_cost:pd.DataFrame, economy:str='Economy 5'):
     upfront_cost = appliance['upfront_cost']
     running_cost = half_hour_cost['cost'].sum()
     maintenance_cost = np.array([float(x) for x in appliance['annual_maintenance_cost'].split("-")])
@@ -66,7 +64,20 @@ def projected_yearly_cost_range(appliance:pd.Series, half_hour_cost:pd.DataFrame
     yearly_cost = lifetime_cost / lifespan[::-1]
 
     carbon_emission = half_hour_cost['carbon_emission'].sum() / 1000 / lifespan[::-1]
+    yearly_maintenance_cost = maintenance_cost / lifespan[::-1]
+
+    # normalise all ranges into a single value
+    upfront_cost = np.mean(upfront_cost)
+    yearly_maintenance_cost = np.mean(yearly_maintenance_cost)
+    yearly_cost = np.mean(yearly_cost)
+    carbon_emission = np.mean(carbon_emission)
+    lifespan = np.mean(lifespan)
+
     return {
+        'economic_type': economy,
+        'upfront_cost': upfront_cost,
+        'lifetime': lifespan,
+        'yearly_maintenance_cost': yearly_maintenance_cost,
         'yearly_cost_range': yearly_cost,
         'yearly_carbon_emission': carbon_emission,
     }
@@ -75,24 +86,28 @@ def projected_yearly_cost_range(appliance:pd.Series, half_hour_cost:pd.DataFrame
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--economy', type=str, default='Economy 5')
-    parser.add_argument('--loadcurve', type=str, default='../business-1-utility-electricity-loadcurve.csv')
-    parser.add_argument('--output', type=str, default='../business-1-utility-gas-costs.csv')
-    parser.add_argument('--appliance_id', type=str, default='1')
+    parser.add_argument('--loadcurve', type=str, default='business-1-appliance-loadcurves.csv')
+    parser.add_argument('--output', type=str, default='business-1-utility-gas-costs.csv')
+    parser.add_argument('--appliance_id', type=str, default=1)
     args = parser.parse_args()
 
-    usage = pd.read_csv(args.loadcurve)
-    tariffs = pd.read_csv('../economy_electricity_tariffs.csv').set_index('economy_type')
+    tariffs = pd.read_csv('economy_electricity_tariffs.csv').set_index('economy_type')
+    load_curve = pd.read_csv(args.loadcurve)
 
-    usage = half_hour_running_cost(usage, tariffs, args.economy)
-    usage.to_csv(args.output, index=False)
+    appliances = pd.read_csv('appliances.csv',)
+    appliance = appliances.loc[int(args.appliance_id)]
+    appliance_type = appliance['appliance']
 
-    appliances = pd.read_csv('../appliances.csv', )
-    appliance = appliances.iloc[int(args.appliance_id)]
-    projected_yearly_cost = projected_yearly_cost_range(appliance, usage)
+    load_curve = load_curve[['business_id', 'mpxn', 'timestamp', 'total_consumption', appliance_type]]
+
+    half_hour_cost = half_hour_running_cost(load_curve, tariffs, usage_column=appliance_type, economy=args.economy)
+    half_hour_cost.to_csv(args.output, index=False)
+    projected_yearly_cost = projected_yearly_cost_range_single_appliance(appliance, half_hour_cost, args.economy)
     yearly_cost_range = projected_yearly_cost['yearly_cost_range']
     yearly_carbon_emission_range = projected_yearly_cost['yearly_carbon_emission']
 
     print(appliance)
+    print(f'Economy type: {args.economy}')
     print(f'Yearly cost range: {yearly_cost_range[0]:.2f}-{yearly_cost_range[1]:.2f}')
     print(f'Yearly Carbon emission range (kg): {yearly_carbon_emission_range[0]:.2f}-'
           f'{yearly_carbon_emission_range[1]:.2f}')
